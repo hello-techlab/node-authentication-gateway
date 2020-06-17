5a2c704837a3820940f8c48369fbbaa7f42bc214
7Nic <gnicolau@nicolau.localdomain>
2020-06-17 16:05:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+// VOCÊ FARÁ ASSIM: NGINX --> API-GATEWAY(NODE) --> SERVIÇO DESEJADO
+// Pegue desse tutorial como fazer o gateway em nodejs: https://www.luiztools.com.br/post/api-gateway-em-arquitetura-de-microservices-com-node-js/
+// A api gateway que fará a autenticação e autorização (verificação da identidade do request), então será nesse serviço MediaStreamAudioSourceNode, usando o google strategy
+require('dotenv-safe').config();
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const helmet = require('helmet');
+var httpProxy = require('express-http-proxy');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
+require('./passport-setup');
+
+app.use(helmet()); //Camada de proteção para requisições maliciosas
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cookieSession({
+  name: 'techlab-session',
+  keys: ['key1', 'key2']
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ===========================================
+app.get('/test/clientes', (req, res, next) => {
+  // apiQqrProxy('')
+});
+
+app.get('/clientes', verifyJWT, (req, res, next) => { 
+  //Aqui mandarei a requisição para o serviço que for necessário com o id do cliente que obtenho de req.UserId
+
+
+  console.log("Retornou todos clientes!");
+  res.json([{id:req.userId ,nome:'Nicolau'}]);
+}); 
+
+// ===========================================
+// Essa glr de baixo vai para outro arquivo
+
+app.post('/login', (req, res, next) => {
+  //esse teste abaixo deve ser feito no seu banco de dados
+  console.log('ok');
+  if(req.body.user === 'nic' && req.body.pwd === '123'){
+    //auth ok
+    const id = 1; //esse id viria do banco de dados
+    const token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 60*5 // expires in 5min
+    })
+    return res.json({ auth: true, token }); //Você retorna para usar manualmente, mas não salva em cookie
+  }
+  
+  res.status(500).json({message: 'Login inválido!'});
+});
+
+function verifyJWT(req, res, next) {
+  const token = req.headers['x-access-token'];
+  if (!token) return res.status(401).json({ auth: false, message: 'No token provided.'});
+
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
+    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.'});
+
+    //Se tudo estiver ok, salvar na request para uso posterior
+    req.userId = decoded.id;
+    next();
+  })  
+}
+// ===========================================
+
+app.get('/auth/failed', (req, res) => {
+  res.send('You failed to authenticate');
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    //Somente retornaremos o jwt para o front, o front deverá armazenar isso e enviar nas próximas requisições
+    res.redirect('/auth/generatejwt');
+  }
+);
+
+app.get('/auth/generatejwt', (req, res, next) => {
+  const token = jwt.sign({ id: req.user.id }, process.env.SECRET, {
+    expiresIn: 60*5 // expires in 5min
+  });
+  //console.log(`Welcome, Mr ${req.user.displayName} of email: ${req.user._json.email} of domain ${req.user._json.hd} and id:${req.user.id}. Your token is: ${token}`);
+  res.status(200).json({auth: true, jwt: token, message: 'Use this token on next requests to identify which user is requesting'});
+});
+
+app.get('/auth/logout', (req, res) => {
+  req.session = null; //Destroy the session
+  req.logout();
+  res.json({ auth: false, token: null });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
---
+++ passport-setup.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.serializeUser(function(user, done) {
+  console.log('serialize');
+  //Use just the id to make the cookie small
+  //I think this is to save the user/user.id on request object
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  console.log('deserialize');
+  //Here the DB function to find the user
+  // User.findById(id, function(err, user) {
+    done(null, user);
+  // });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: "1059970612750-69c6kp70e9drthvvtd0r94od8ghphr1o.apps.googleusercontent.com",
+    clientSecret: "cr1CMX3wSk6n5aduExkc1j6u",
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(acessToken, refreshToken, profile, done) {
+    console.log('passport use');
+    //Usar o BD para achar ou criar um user
+    // profile.findOrCreate({ googleId: profile.id, }), function(err, profile) {
+      //Use the profile info to check if this user is regitered in my db. You can use the id
+      return done(null, profile);
+      // return done(null, profile);
+    // }
+  }
+));
