2111bd31f7b89ef2e2bf05b9566dc412d559dd1f
7Nic <gnicolau@nicolau.localdomain>
2020-06-17 16:22:28 -0300

    separating authentication file

--- authentication.js
+++ authentication.js
@@ -1,52 +1,5 @@
-// VOCÊ FARÁ ASSIM: NGINX --> API-GATEWAY(NODE) --> SERVIÇO DESEJADO
-// Pegue desse tutorial como fazer o gateway em nodejs: https://www.luiztools.com.br/post/api-gateway-em-arquitetura-de-microservices-com-node-js/
-// A api gateway que fará a autenticação e autorização (verificação da identidade do request), então será nesse serviço MediaStreamAudioSourceNode, usando o google strategy
-require('dotenv-safe').config();
-const jwt = require('jsonwebtoken');
 const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const helmet = require('helmet');
-var httpProxy = require('express-http-proxy');
-const cookieSession = require('cookie-session');
-const cookieParser = require('cookie-parser');
-require('./passport-setup');
-
-app.use(helmet()); //Camada de proteção para requisições maliciosas
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cookieSession({
-  name: 'techlab-session',
-  keys: ['key1', 'key2']
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// ===========================================
-const apiQqrProxy = httpProxy('http://localhost:8080');
-app.get('/test/clientes', (req, res, next) => {
-  apiQqrProxy(req, res, next);
-});
-
-app.post('/test', verifyJWT, (req, res, next) => {
-  apiQqrProxy(req, res, next);
-});
-
-app.get('/clientes', verifyJWT, (req, res, next) => { 
-  //Aqui mandarei a requisição para o serviço que for necessário com o id do cliente que obtenho de req.UserId
-
-
-  console.log("Retornou todos clientes!");
-  res.json([{id:req.userId, nome:'Nicolau'}]);
-}); 
-
-// ===========================================
-// Essa glr de baixo vai para outro arquivo
+const authRouter = express.Router();
 
 app.post('/login', (req, res, next) => {
   //esse teste abaixo deve ser feito no seu banco de dados
@@ -106,6 +59,3 @@ app.get('/auth/logout', (req, res) => {
   req.logout();
   res.json({ auth: false, token: null });
 });
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
