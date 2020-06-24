bc9227e2fb1f998b8e462fa416a317225d750eef
7Nic <gnicolau@nicolau.localdomain>
2020-06-24 14:57:09 -0300

    Sign up and login routes added. First login logic added

--- authentication.js
+++ authentication.js
@@ -1,30 +1,69 @@
 const express = require('express');
+const url = require('url');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const authRouter = express.Router();
 
 authRouter.get('/failed', (req, res) => {
-  res.send('You failed to authenticate');
+  res.send('You failed to authenticate. You do not have an USP email :(');
 });
 
-authRouter.get('/google',
+authRouter.get('/login',
   passport.authenticate('google', { scope: ['profile', 'email'] })
 );
+
+authRouter.get('/signup', (req, res, next) => {
+  res.send('Aqui teremos a página de cadastro dos dados, perguntaremos o instituto e o número usp da pessoa. Essa página só aparecerá no primeiro login');
+});
+
+authRouter.post('/signup', (req, res, next) => {
+  
+  //Dados criados pelo usuário no cadastro
+  let nusp = req.body.nusp;
+  let instituto = req.body.instituto;
+
+  //Dados retornados pelo oAuth
+  let userId = req.userId; 
+  let userName = req.userName;
+  let userEmail = req.userEmail;
+
+  //Aqui chamaremos a função para criar o usuário no BD com as informações que conseguimos acima
+});
 
 authRouter.get('/google/callback', 
   passport.authenticate('google', { failureRedirect: '/failed' }),
   function(req, res) {
-    //Somente retornaremos o jwt para o front, o front deverá armazenar isso e enviar nas próximas requisições
+    
+    //Se o domínio não for USP, rejeitaremos a requisição de login
+    if (req.user._json.hd !== 'usp.br') res.redirect('/auth/failed');
+
+    // ======== AQUI VERIFICAREMOS SE O USUÁRIO JÁ EXISTE NO BD, se existir o mandaremos para '/auth/generatejwt'
+    // se não existir o mandaremos para '/auth/signup' com um GET
+
+    //Retornaremos o jwt para o front, o front deverá armazenar isso e enviar nas próximas requisições
     res.redirect('/auth/generatejwt');
   }
 );
 
 authRouter.get('/generatejwt', (req, res, next) => {
-  const token = jwt.sign({ id: req.user.id }, process.env.SECRET, {
+  const token = jwt.sign({ 
+    id: req.user.id,
+    name: req.user.displayName,
+    email: req.user._json.email,
+    hd: req.user._json.hd
+   }, process.env.SECRET, {
     expiresIn: 60*60*24 // expires in 1 day
   });
-  //console.log(`Welcome, Mr ${req.user.displayName} of email: ${req.user._json.email} of domain ${req.user._json.hd} and id:${req.user.id}. Your token is: ${token}`);
-  res.status(200).json({auth: true, jwt: token, message: 'Use this token on next requests to identify which user is requesting'});
+
+  // res.status(200).json({auth: true, jwt: token, message: 'Use this token on next requests to identify which user is requesting'});
+  res.redirect(url.format({
+    pathname:"/",
+    query: {
+       "auth": true,
+       "token": token,
+       "message": 'Use this token on next requests to identify which user is requesting'
+     }
+  }));
 });
 
 authRouter.get('/logout', (req, res) => {
--- index.js
+++ index.js
@@ -118,15 +118,27 @@ app.delete('/usuarios/gapsi/:nuspusuario', verifyJWT, (req, res, next) => {
 const PORT = process.env.port | 8080;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
-function verifyJWT(req, res, next) {
+async function verifyJWT(req, res, next) {
   const token = req.headers['x-access-token'];
-  if (!token) return res.status(401).json({ auth: false, message: 'No token provided.'});
+
+  // if (!token) return res.status(401).json({ auth: false, message: 'No token provided.'});
+  if (!token) res.redirect('/auth/login');
 
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
-    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.'});
 
-    //Se tudo estiver ok, salvar na request para uso posterior
+    // if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.'});
+    if (err) res.redirect('/auth/login');
+
+    //Se tudo estiver ok, salvar dados na request para uso posterior em outros serviços
     req.userId = decoded.id;
+    req.userName = decoded.name;
+    req.userEmail = decoded.email;
+    req.hostDomain = decoded.hd;
+
+    //Se não for um email usp rejeitaremos
+    if (req.hostDomain !== 'usp.br') res.redirect('/auth/failed');
+
+    console.log(decoded);
     next();
-  })  
+  });  
 }
