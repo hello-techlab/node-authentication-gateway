b0593ffac3da36d65a9413cdb98b349aed2990a8
7Nic <gnicolau@nicolau.localdomain>
2020-06-26 19:50:28 -0300

    fixing bugs

--- passport-setup.js
+++ passport-setup.js
@@ -3,7 +3,6 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.serializeUser(function(user, done) {
   console.log('serialize');
-  //Use just the id to make the cookie small
   //I think this is to save the user/user.id on request object
   console.log(user);
   done(null, user);
@@ -20,8 +19,8 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: "1059970612750-69c6kp70e9drthvvtd0r94od8ghphr1o.apps.googleusercontent.com",
     clientSecret: "cr1CMX3wSk6n5aduExkc1j6u",
-    // callbackURL: `http://techlab-oauth.mooo.com/auth/google/callback`
-    callbackURL: "http://localhost:8080/auth/google/callback"
+    callbackURL: `http://techlab-oauth.mooo.com/auth/google/callback`
+    // callbackURL: "http://localhost:8080/auth/google/callback"
   },
   function(acessToken, refreshToken, profile, done) {
     console.log('passport use');
